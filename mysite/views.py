2e508f1079fad52f4bfe6368197fcf08de170e68
colin <colin2328@gmail.com>
2013-10-29 16:14:27 -0400

    finished tutorial through templates

--- mysite/views.py
+++ mysite/views.py
@@ -1,20 +1,19 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
 
 
 def hello(request):
-	return HttpResponse("Hello World")
+    return HttpResponse("Hello World")
 
 def current_datetime(request):
-	now = datetime.datetime.now()
-	html = "<html><body>It is now %s.</body></html>" % now
-	return HttpResponse(html)
+    now = datetime.datetime.now()
+    return render(request, 'current_datetime.html', {'current_date': now})
 
 def hours_ahead(request, offset):
-	try:
-		offset = int(offset)
-	except ValueError:
-		raise Http404()
-	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-	html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-	return HttpResponse(html)
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    return render(request, 'hours_ahead.html', {'next_time' : dt, 'hour_offset' : str(offset)})
